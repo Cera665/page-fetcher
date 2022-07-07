50db119f89df2175cbb0470cf26f410fea6757fe
Cera665 <deletedunreadxoxo@gmail.com>
2022-07-07 19:11:35 -0400

    And again

--- fetcher.js
+++ fetcher.js
@@ -1,7 +1,25 @@
 const request = require('request');
 const fs = require('fs');
 
+const localPath = process.argv[3];
+const urlToFetch = process.argv[2];
 
+request(urlToFetch, (error, response, bosy) => {
+  if (error) {
+    console.log("Error:", error);
+  }
+  fs.writeFile(`$localPath}`, bosy, function(error) {
+    if (error) {
+      console.log("Error:", error);
+    } else {
+      console.log(`${body.length} bytes to ${localPath}`);
+    }
+  });
+});
+
+
+
+/* first try was not working!
 const args = process.argv.slice(2);
 let urlToFetch = args[0];
 let localPath = args[1];
@@ -19,3 +37,4 @@ const writeFetched = function() {
     }
   });
 };
+*/
